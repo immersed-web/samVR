a2bf2c488244b18c0f561186cfbab32b09863e83
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-06-04 15:51:33 +0200

    added some test code for checking how debug-level behaves

--- backend/tsx-test/src/index.ts
+++ backend/tsx-test/src/index.ts
@@ -1,10 +1,18 @@
+process.env.DEBUG_LEVEL = 'DEBUG';
 import { db, schema } from 'database';
+import { Log } from 'debug-level';
+const uLog = new Log('u');
+process.env.DEBUG = 'u*, ' + process.env.DEBUG;
+uLog.enable(process.env.DEBUG);
 
 const response = await db.query.users.findFirst({
   // where: (users, { eq }) => eq(users.username, 'klas'),
 });
 
-console.log(response)
+// console.log(response)
+uLog.debug(response);
+uLog.log(response);
+uLog.info(response);
 
 
 process.exit(0);
