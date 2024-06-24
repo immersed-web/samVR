0cba8fb56c813263e412aa1cdd448a3f166a17d7
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-06-24 12:56:23 +0200

    WIP: loading aframe and wrap in a-scene. provide scenetag and domoutlet

--- app/src/views/user/VrSpaceSceneView.vue
+++ app/src/views/user/VrSpaceSceneView.vue
@@ -2,23 +2,41 @@
   <div>
     <div class="min-h-screen z-0">
       <h2>VrSpace main scene</h2>
-      <!-- <VrAFrame v-if="streamStore.modelUrl" :model-url="streamStore.modelUrl" :navmesh-url="streamStore.navmeshUrl"
-        :model-scale="streamStore.currentStream?.vrSpace?.virtualSpace3DModel?.scale" /> -->
+      <div class="pointer-events-none *:pointer-events-auto" ref="domOutlet" id="aframe-dom-outlet" />
+      <a-scene ref="sceneTag">
+        <VrAFrame v-if="vrSpaceStore.worldModelUrl" :show-nav-mesh="false" />
+      </a-scene>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 // import { onMounted } from 'vue';
+import { aFrameSceneProvideKey } from '@/modules/injectionKeys';
 import VrAFrame from '../../components/lobby/VrAFrame.vue';
-// import { useVrSpaceStore } from '@/stores/vrSpaceStore';
-import { useStreamStore } from '@/stores/streamStore';
+import { useVrSpaceStore } from '@/stores/vrSpaceStore';
+import type { VrSpaceId } from 'schemas';
+import { onBeforeMount, provide, ref } from 'vue';
+import type { Scene } from 'aframe';
 
-const streamStore = useStreamStore();
-// const vrSpaceStore = useVrSpaceStore();
-// onMounted(async () => {
-//   await vrSpaceStore.enterVrSpace();
-// });
+const vrSpaceStore = useVrSpaceStore();
+
+const props = defineProps<{
+  vrSpaceId: VrSpaceId
+}>()
+const sceneTag = ref<Scene>();
+const domOutlet = ref<HTMLDivElement>();
+provide(aFrameSceneProvideKey, { sceneTag, domOutlet });
+
+onBeforeMount(async () => {
+  await import('aframe');
+  const { default: c } = await import('@/ts/aframe/components');
+  c.registerAframeComponents();
+})
+
+onBeforeMount(async () => {
+  await vrSpaceStore.enterVrSpace(props.vrSpaceId);
+});
 
 </script>
 
