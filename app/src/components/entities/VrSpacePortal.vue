1af4da7bac5cc0eb27d5a57e397192a44dbb4643
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-06-25 15:56:51 +0200

    portal hover animation

--- app/src/components/entities/VrSpacePortal.vue
+++ app/src/components/entities/VrSpacePortal.vue
@@ -1,7 +1,10 @@
 <template>
   <a-entity>
     <a-troika-text v-if="props.label" look-at-camera :value="props.label" position="0 2.5 0" />
-    <a-sphere @click="emit('click')" :class="[props.clickable ? 'clickable' : '']" transparent="true"
+    <a-sphere
+      animation__hover="property: scale; startEvents: mouseenter; easing: easeInCubic; dur: 150; from: 0.5 0.5 0.5; to: 0.7 0.7 0.7"
+      animation__leave="property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 150; from: 0.7 0.7 0.7; to: 0.5 0.5 0.5"
+      @click="emit('click', $event)" :class="[props.clickable ? 'clickable' : '']" transparent="true"
       scale="0.5 0.5 0.5" material="shader: outer-glow; start: 0.3; color: 0.5 0 1;" position="0 1.5 0">
       <a-icosahedron v-if="props.panoramicPreviewUrl" detail="5" scale="-0.98 -0.98 -0.98" transparent="true"
         opacity="0.5"
