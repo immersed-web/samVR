c58e3f86f5e7b48553c63d063b626cfa8b907f5c
Martin Törnros <martin.tornros@ri.se>
2024-08-21 10:29:17 +0200

    Login view design

--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,77 +1,97 @@
 <template>
   <div>
-    <div class="min-h-screen hero bg-base-200">
-      <div class="flex-col gap-10 hero-content lg:flex-row-reverse">
-        <div
-          class="mt-4"
-          v-once
-          v-if="showDevLoginButtons"
-        >
-          <h2>Devmode quick login</h2>
-          <div class="space-x-2 mt-4 text-right">
-            <button
-              @click="loginDetails('sender','123')"
-              class="btn btn-primary btn-outline"
-            >
-              Sender
-            </button>
-            <button
-              @click="loginDetails('superadmin','bajskorv')"
-              class="btn btn-primary btn-outline"
-            >
-              Superadmin
-            </button>
+    <!-- <div class="min-h-screen hero bg-base-200"> -->
+    <div class="flex flex-col lg:flex-row-reverse min-h-screen">
+      <!-- <div class="flex-row lg:flex-row gap-10 "> -->
+
+      <!-- Info text -->
+      <div class="flex-1 hero">
+        <div class="card">
+          <div class="card-body">
+            <h1 class="mb-2">
+              SamVR
+            </h1>
+            <div>
+              <p class="font-bold mb-2">
+                En säker plats för elever att utforska, lära och skapa utan bekymmer om personlig integritet.
+              </p>
+              <p>
+                SamVR låter elever interagera med olika objekt såsom bilder, text, filmer, länkar och till
+                och med dela sina skärmar - allt med GDPR-säkerhet i åtanke. Kliv in i ett galleri där du och dina
+                klasskamrater kan ta del av och lära sig av varandras redovisningar på ett helt nytt sätt!
+              </p>
+            </div>
           </div>
         </div>
-        <!-- <VenueListView /> -->
-        <div
-          class="flex-shrink-0 max-w-sm shadow-2xl card bg-base-100"
-        >
-          <form
-            class="card-body"
-            @submit.prevent="login"
-          >
-            <h2>Logga in</h2>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Användarnamn</span>
-              </label>
-              <input
-                v-model="username"
-                type="text"
-                placeholder="Användarnamn"
-                class="input input-bordered"
-              >
+      </div>
+
+      <!-- Login -->
+      <div class="flex-1 hero"
+        style="background-image: url(https://plus.unsplash.com/premium_photo-1663091704223-cc051e0f0c47?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);">
+        <div class="hero-content max-w-md">
+          <div>
+            <div class="shadow-2xl card bg-base-100">
+              <div class="card-body">
+                <form class="" @submit.prevent="login">
+                  <h2 class="mb-2">
+                    Logga in
+                  </h2>
+                  <div class="form-control">
+                    <label class="label">
+                      <span class="label-text">Användarnamn</span>
+                    </label>
+                    <input v-model="username" type="text" placeholder="Användarnamn" class="input input-bordered">
+                  </div>
+                  <div class="form-control">
+                    <label class="label">
+                      <span class="label-text">Lösenord</span>
+                    </label>
+                    <input v-model="password" type="password" placeholder="Lösenord" class="input input-bordered">
+                  </div>
+                  <div v-if="error" class="alert bg-error">
+                    {{ error }}
+                  </div>
+                  <div class="mt-6 form-control">
+                    <button type="submit" class="btn btn-primary">
+                      Logga in
+                    </button>
+                  </div>
+                </form>
+                <div v-once v-if="showDevLoginButtons" class="space-x-2 text-sm">
+                  <div class="divider">
+                    Hackare?
+                  </div>
+                  Smit in som:
+                  <!-- <div class="space-x-2"> -->
+                  <button @click="loginDetails('klas', '123')" class="btn btn-xs btn-primary btn-outline">
+                    Klas <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                      <path fill-rule="evenodd"
+                        d="M5 4a.75.75 0 0 1 .738.616l.252 1.388A1.25 1.25 0 0 0 6.996 7.01l1.388.252a.75.75 0 0 1 0 1.476l-1.388.252A1.25 1.25 0 0 0 5.99 9.996l-.252 1.388a.75.75 0 0 1-1.476 0L4.01 9.996A1.25 1.25 0 0 0 3.004 8.99l-1.388-.252a.75.75 0 0 1 0-1.476l1.388-.252A1.25 1.25 0 0 0 4.01 6.004l.252-1.388A.75.75 0 0 1 5 4ZM12 1a.75.75 0 0 1 .721.544l.195.682c.118.415.443.74.858.858l.682.195a.75.75 0 0 1 0 1.442l-.682.195a1.25 1.25 0 0 0-.858.858l-.195.682a.75.75 0 0 1-1.442 0l-.195-.682a1.25 1.25 0 0 0-.858-.858l-.682-.195a.75.75 0 0 1 0-1.442l.682-.195a1.25 1.25 0 0 0 .858-.858l.195-.682A.75.75 0 0 1 12 1ZM10 11a.75.75 0 0 1 .728.568.968.968 0 0 0 .704.704.75.75 0 0 1 0 1.456.968.968 0 0 0-.704.704.75.75 0 0 1-1.456 0 .968.968 0 0 0-.704-.704.75.75 0 0 1 0-1.456.968.968 0 0 0 .704-.704A.75.75 0 0 1 10 11Z"
+                        clip-rule="evenodd" />
+                    </svg>
+                  </button>
+                  <button @click="loginDetails('göran', '123')" class="btn btn-xs btn-primary btn-outline">
+                    Göran <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor"
+                      class="size-4">
+                      <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                  </button>
+                  <button @click="loginDetails('fia', '123')" class="btn btn-xs btn-primary btn-outline">
+                    Fia <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4">
+                      <path
+                        d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                    </svg>
+                  </button>
+                  <!-- </div> -->
+                </div>
+              </div>
             </div>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Lösenord</span>
-              </label>
-              <input
-                v-model="password"
-                type="password"
-                placeholder="Lösenord"
-                class="input input-bordered"
-              >
-            </div>
-            <div
-              v-if="error"
-              class="alert bg-error"
-            >
-              {{ error }}
-            </div>
-            <div class="mt-6 form-control">
-              <button
-                type="submit"
-                class="btn btn-primary"
-              >
-                Logga in
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
       </div>
     </div>
+    <!-- </div> -->
   </div>
 </template>
 
@@ -87,6 +107,7 @@ import { useConnectionStore } from '@/stores/connectionStore';
 // import StreamListView from '@/views/public/StreamListView.vue';
 
 const showDevLoginButtons = import.meta.env.DEV;
+// const showDevLoginButtons = false;
 
 // Router
 const router = useRouter();
