a79bf390f45060c6fc879422806d00c2b7578e27
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-09-16 14:02:21 +0200

    placed images have correct proportions

--- app/src/components/lobby/PlacedAsset.vue
+++ app/src/components/lobby/PlacedAsset.vue
@@ -1,14 +1,24 @@
 <template>
   <a-entity>
-    <component :is="tagName" :src="src" />
+    <a-image @materialtextureloaded="onTextureLoaded" v-if="tagName === 'a-image'" :src="src" />
+    <component v-else :is="tagName" :src="src" />
   </a-entity>
 </template>
 <script setup lang="ts">
 import { getAssetUrl } from '@/modules/utils';
+import type { ComponentDefinition, ComponentDescriptor, DetailEvent, THREE, components } from 'aframe';
 import { extensionsToAframeTagsMap, type Asset } from 'schemas';
 import { computed } from 'vue';
 
 
+function onTextureLoaded(event: DetailEvent<{ src: HTMLImageElement, texture: THREE.Texture }>) {
+  console.log(event);
+  const aImageTag = event.target;
+  const { width, height } = event.detail.src
+  const ratio = width / height;
+  aImageTag.object3D.scale.set(ratio, 1, 1);
+  return
+}
 
 const tagName = computed(() => {
   return extensionsToAframeTagsMap[props.asset.assetFileExtension];
