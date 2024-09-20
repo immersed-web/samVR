18574fe82bc51dcfa24570c30c48e2ca7a211308
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-09-20 08:32:33 +0200

    fix: event handlers in raycast-update are removed and attached when needed. The event handlers are attached to canvas instead of document.

--- app/src/ts/aframe/raycaster-update.ts
+++ app/src/ts/aframe/raycaster-update.ts
@@ -11,26 +11,41 @@ export default function () {
     prev: undefined as THREE.Vector3 | undefined,
     stashedCursorStyle: undefined as string | undefined,
     outsideWindow: false,
+    onMouseEnter(e: MouseEvent) {
+      if (e instanceof MouseEvent) {
+        console.log('cursor entered the canvas', e);
+        this.outsideWindow = false;
+      }
+    },
+    onMouseLeave(e: MouseEvent) {
+      if (e instanceof MouseEvent) {
+        // this.el.components.raycaster.data.enabled = false;
+        // console.log(this.el.components.raycaster);
+        console.log('cursor left the canvas', e);
+        this.outsideWindow = true;
+      }
+    },
     init: function () {
+      this.onMouseEnter = this.onMouseEnter.bind(this);
+      this.onMouseLeave = this.onMouseLeave.bind(this);
       console.log('INIT raycaster-update');
       this.prev = new THREE.Vector3();
       this.tick = AFRAME.utils.throttleTick(this.tick, 10, this);
+      const canvas = this.el.sceneEl!.canvas;
 
-      document.documentElement.addEventListener('mouseleave', (e) => {
-        if (e instanceof MouseEvent) {
-          // this.el.components.raycaster.data.enabled = false;
-          // console.log(this.el.components.raycaster);
-          console.log('cursor left the canvas', e);
-          this.outsideWindow = true;
-        }
-      })
-      document.documentElement.addEventListener('mouseenter', (e) => {
-        if (e instanceof MouseEvent) {
-          console.log('cursor entered the canvas', e);
-          this.outsideWindow = false;
-        }
-      })
+      canvas.addEventListener('mouseenter', this.onMouseEnter)
+      canvas.addEventListener('mouseleave', this.onMouseLeave)
 
+    },
+    remove() {
+      const canvas = this.el.sceneEl!.canvas;
+      canvas.removeEventListener('mouseenter', this.onMouseEnter)
+      canvas.removeEventListener('mouseleave', this.onMouseLeave)
+    },
+    pause() {
+      const canvas = this.el.sceneEl!.canvas;
+      canvas.removeEventListener('mouseenter', this.onMouseEnter)
+      canvas.removeEventListener('mouseleave', this.onMouseLeave)
     },
     events: {
       'raycaster-intersection': function (evt: DetailEvent<unknown>) {
