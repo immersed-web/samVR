05f1dea52935e83ce278b31a121a78419fde7a3e
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-10-09 10:34:48 +0200

    placing video works

--- app/src/components/lobby/PlacedAsset.vue
+++ app/src/components/lobby/PlacedAsset.vue
@@ -4,6 +4,8 @@
     <a-image @materialtextureloaded="onTextureLoaded" v-if="tagName === 'a-image'" :class="$attrs.class" :src="src" />
     <PdfEntity v-else-if="tagName === 'PdfEntity'" v-model:current-page="currentPage" :src="src"
       :class="$attrs.class" />
+    <a-video v-else-if="tagName === 'a-video'" :src="src" :class="$attrs.class"
+      @materialtextureloaded="onVideoTextureLoaded" @materialvideoloadeddata="onVideoTextureLoaded" />
     <component rotation="90 0 0" v-else :is="tagName" :src="src" :class="$attrs.class" />
   </a-entity>
 </template>
@@ -29,8 +31,19 @@ onUnmounted(() => {
 
 const currentPage = ref(1);
 
+function onVideoTextureLoaded(evt: DetailEvent<{ src: HTMLVideoElement, texture: THREE.VideoTexture }>) {
+  // console.log('videoTexture loaded:', evt);
+  const aVideoTag = evt.target;
+  const videoElement = evt.detail.src;
+  videoElement.loop = true;
+  const { videoWidth, videoHeight } = videoElement;
+  const ratio = videoWidth / videoHeight;
+  aVideoTag.object3D.scale.set(ratio, 1, 1);
+  videoElement.play();
+}
+
 function onTextureLoaded(event: DetailEvent<{ src: HTMLImageElement, texture: THREE.Texture }>) {
-  console.log('texture loaded');
+  // console.log('texture loaded:', event);
   const aImageTag = event.target;
   const { width, height } = event.detail.src
   const ratio = width / height;
