b91d93ee9159c3d9363c0353dbadbf0c346333bc
Gunnar Oledal <gunnar.oledal@gmail.com>
2024-10-31 12:21:19 +0000

    show description in vrList

--- app/src/components/VRList.vue
+++ app/src/components/VRList.vue
@@ -8,42 +8,38 @@
       </template>
       <template v-if="canCreateVrSpace">
         <button class="btn btn-primary self-start" @click="createVrSpace">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-            stroke="currentColor" class="size-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-          </svg>
+          <span class="material-icons">add</span>
           Skapa ny VR-scen
         </button>
       </template>
     </div>
-    <div class="w-full grid grid-cols-[repeat(auto-fill,minmax(14rem,1fr))] gap-4">
-      <div class="flex gap-2 items-center" v-for="space in listedVrSpaces" :key="space.vrSpaceId">
-        <div class="card card-compact w-full shadow-xl">
-          <figure class="w-full h-32" style="background-size: cover"
-            :style="{ 'background-image': 'url(' + (space.image ? getAssetUrl(space.image) : 'https://plus.unsplash.com/premium_photo-1663091704223-cc051e0f0c47?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') + ')' }" />
-          <div class="card-body bg-base-400 ">
-            <h2 class="card-title">
-              {{ space.name }}
-            </h2>
-            <div>
-              <div>Synlighet: {{ space.visibility }}</div>
-              <div>Din rättighetsnivå: {{ space.permissionLevel }}</div>
-            </div>
-            <div class="card-actions justify-end">
-              <RouterLink tag="button"
-                v-if="space.permissionLevel && hasAtLeastPermissionLevel(space.permissionLevel, 'edit')"
-                :to="{ name: 'vrSpaceSettings', params: { vrSpaceId: space.vrSpaceId } }" class="btn btn-sm">
-                Redigera
-              </RouterLink>
-              <RouterLink tag="button" class="btn btn-primary btn-sm"
-                :to="{ name: 'vrSpace', params: { vrSpaceId: space.vrSpaceId } }">
-                Besök
-              </RouterLink>
-            </div>
+    <div class="w-full grid grid-cols-[repeat(auto-fill,minmax(14rem,1fr))] gap-4 items-stretch">
+      <div v-for="space in listedVrSpaces" :key="space.vrSpaceId" class="card card-compact w-full shadow-xl">
+        <figure class="w-full h-32" style="background-size: cover"
+          :style="{ 'background-image': 'url(' + (space.image ? getAssetUrl(space.image) : 'https://plus.unsplash.com/premium_photo-1663091704223-cc051e0f0c47?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') + ')' }" />
+        <div class="card-body bg-base-400 ">
+          <h2 class="card-title">
+            {{ space.name }}
+          </h2>
+          <div class="grow">
+            <div>Synlighet: {{ space.visibility }}</div>
+            <div>Din rättighetsnivå: {{ space.permissionLevel }}</div>
+            <div v-if="space.description">{{ space.description }}</div>
+          </div>
+          <div class="card-actions justify-end">
+            <RouterLink tag="button"
+              v-if="space.permissionLevel && hasAtLeastPermissionLevel(space.permissionLevel, 'edit')"
+              :to="{ name: 'vrSpaceSettings', params: { vrSpaceId: space.vrSpaceId } }" class="btn btn-sm">
+              Redigera
+            </RouterLink>
+            <RouterLink tag="button" class="btn btn-primary btn-sm"
+              :to="{ name: 'vrSpace', params: { vrSpaceId: space.vrSpaceId } }">
+              Besök
+            </RouterLink>
           </div>
         </div>
-        <!-- <pre>{{ space }}</pre> -->
       </div>
+      <!-- <pre>{{ space }}</pre> -->
     </div>
   </div>
 </template>
