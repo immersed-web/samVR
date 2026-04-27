cbff5e102801e77f8433cee6f500493635eb2c1a
Darkdusk <johan.svensson.01@hotmail.com>
2026-04-27 10:21:20 +0200

    Linked the buttons to the functions and changed text color

--- app/src/components/EscapeMenu.vue
+++ app/src/components/EscapeMenu.vue
@@ -10,14 +10,14 @@
       </form>
       
       <div class="modal-box bg-base-100/10 backdrop-blur-xl border border-base-200/50 max-w-lg">
-        <h3 class="text-2xl font-bold text-base-content mb-6">VR Menu</h3>
+        <h3 class="text-2xl font-bold text-base-content mb-6 text-white">VR Menu</h3>
         
         <div class="space-y-4">
-            <button class="w-full btn btn-primary btn-wide">Återuppta</button>
-            <button class="w-full btn btn-error btn-wide">Lämna VR miljö</button>
+            <button @click="props.model.onResume" class="w-full btn btn-primary btn-wide">Återuppta</button>
+            <button @click="props.model.onLeave" class="w-full btn btn-neutral btn-wide">Lämna VR miljö</button>
         </div>
         
-        <div class="mt-8 pt-6 border-t border-base-200/50 text-sm text-base-content/70 text-center">
+        <div class="mt-8 pt-6 border-t border-base-200/50 text-sm text-base-content/70 text-center text-white">
           <p><kbd class="kbd kbd-sm">Esc</kbd> to close</p>
         </div>
       </div>
